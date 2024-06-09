555126c70f22b8186f227eb2d58cf72710e64401
Arctixinc <105559773+Arctixinc@users.noreply.github.com>
2024-06-09 22:44:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,20 @@
-from app import create_app
+from flask import Flask
+from routes.home import home_bp
+from routes.details import details
+from routes.examinfo import examinfo
+from routes.results import results
+from routes.sgpa import sgpa
+
+
+app = Flask(__name__)
+
+# Register route blueprints here
+app.register_blueprint(home_bp)
+app.register_blueprint(details)
+app.register_blueprint(examinfo)
+app.register_blueprint(results)
+app.register_blueprint(sgpa)
 
 if __name__ == '__main__':
-    app = create_app()
     app.run(debug=True)
+    
